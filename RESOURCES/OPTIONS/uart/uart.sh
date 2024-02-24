d82582f723f6bf99d12792973d2e7eccca04b914
Alexander <alexxycarlsson@gmail.com>
2024-02-24 04:34:44 +0100

    fix uart

--- RESOURCES/OPTIONS/uart/uart.sh
+++ RESOURCES/OPTIONS/uart/uart.sh
@@ -9,9 +9,6 @@ fi
 project_root="$1"
 uart_package="$2"
 
-# check the required tools
-check_tools "unzip"
-
 # check the project root folder
 if [ ! -d "$project_root" ]; then
   echo -e "${RED}ERROR: Cannot find the folder '$project_root' ${NC}"
@@ -25,26 +22,55 @@ if [ ! -d "$uart_package_folder" ]; then
   exit 4
 fi
 
-# check the uart package file
-uart_package_file="${uart_package_folder}/uboot.zip"
-if [ ! -f "$uart_package_file" ]; then
-  echo -e "${RED}ERROR: Cannot find the file '$uart_package_file' ${NC}"
+# check the uart files uboot and boot0
+uboot_file="${uart_package_folder}/uboot"
+if [ ! -f "$uboot_file" ]; then
+  echo -e "${RED}ERROR: Cannot find the file '$uboot_file' ${NC}"
   exit 5
+fi
+
+boot0_file="${uart_package_folder}/boot0"
+if [ ! -f "$boot0_file" ]; then
+  echo -e "${RED}ERROR: Cannot find the file '$boot0_file' ${NC}"
+  exit 6
 fi
 
 # check the target folder
 target_folder="$ROOTFS_DIR"
 if [ ! -d "$target_folder" ]; then
   echo -e "${RED}ERROR: Cannot find the target folder '$target_folder' ${NC}"
-  exit 6
+  exit 7
 fi
 
-# enable the selected ssh package
-current_folder="$PWD"
-cd "$target_folder" || exit 7
-echo -e "${YELLOW}INFO: Unzipping the UART package ...${NC}"
-unzip -oqq "$uart_package_file"
-cd "$current_folder" || exit 8
+echo -e "${YELLOW}INFO: Copying the uart files ...${NC}"
+
+# Copy the uart files to the target folder
+/bin/cp -f "$uboot_file" "$target_folder"
+
+if [ $? != 0 ]; then
+  echo -e "${RED}ERROR: Failed to copy the uboot file ${NC}"
+  exit 8
+fi
+
+echo -e "${GREEN}INFO: The uboot file has been copied${NC}"
+
+/bin/cp -f "$boot0_file" "$target_folder"
+
+if [ $? != 0 ]; then
+  echo -e "${RED}ERROR: Failed to copy the boot0 file ${NC}"
+  exit 9
+fi
+
+echo -e "${GREEN}INFO: The boot0 file has been copied${NC}"
+
+# Overwrite the inittab file
+echo -e "${YELLOW}INFO: Overwriting the inittab file ...${NC}"
+
+cat <<EOF >"$target_folder/etc/inittab"
+::sysinit:/etc/init.d/rcS S boot
+::shutdown:/etc/init.d/rcS K shutdown
+::askconsole:/bin/ash --login
+EOF
 
 echo -e "${GREEN}INFO: The UART package has been installed. Version: $uart_package ${NC}"
 
